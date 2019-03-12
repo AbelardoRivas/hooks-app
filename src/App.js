6e2fe57faf3cc5a257b2b3c72c525372d4210616
Abelardo Rivas <abelardo.rivas1@gmail.com>
2019-03-12 17:34:23 -0400

    Convirtiendo el componente App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Todo from './components/Todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Todo />
-      </div>
-    );
+const app = props => {
+  const [page, setPage] = useState('auth');
+
+  const swithPage = (pageName) => {
+    setPage(pageName);
   }
-}
 
-export default App;
+  return (
+    <div className="App">
+      <Header 
+        onLoadTodos={swithPage.bind(this, 'todos')} 
+        onLoadAuth={swithPage.bind(this, 'auth')} 
+      />
+      <hr />
+      {page === 'auth' ? <Auth /> : <Todo />} 
+    </div>
+  );
+};
+
+export default app;
